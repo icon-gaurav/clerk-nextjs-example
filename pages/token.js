5db255de95f2a23f419db711ca32310c81d2f580
gaurav <icon.gaurav806@gmail.com>
2022-05-30 16:24:57 +0530

    token page added

---
+++ pages/token.js
@@ -0,0 +1,26 @@
+import { useAuth } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+
+const Token = () => {
+  const { getToken,userId } = useAuth();
+  const [token, setToken] = useState("")
+  console.log(getToken);
+  const fetchToken = async () => {
+    const t = await getToken({ template: "hasura" });
+    console.log(t, userId);
+    setToken(t)
+  };
+  useEffect(() => {
+    fetchToken().then(r => {
+    });
+  }, []);
+
+  return (
+    <div style={{display:"flex", flexDirection:"column", maxWidth:1031, margin:"0 auto", flexWrap:"wrap"}}>
+      <p>Token page</p>
+      <p style={{overflowWrap:"anywhere", padding:10}}>{JSON.stringify(token)}</p>
+    </div>
+  );
+};
+
+export default Token;
